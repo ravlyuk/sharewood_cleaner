a0e5431836b6d318e666aeabb6267aa861b37043
Yevhenii Ravliuk <41680988+ravlyuk@users.noreply.github.com>
2023-07-12 01:01:56 +0300

    Update fix.py

--- fix.py
+++ fix.py
@@ -17,23 +17,30 @@ def remove_substring_from_filename(folder_path, substring):
                 old_file_path = os.path.join(root, file_name)
                 new_file_path = os.path.join(root, new_file_name)
                 os.rename(old_file_path, new_file_path)
-                if new_file_name.strip() in ["SW.BAND - 150000 курсов ждут тебя!.url",
-                                             "DMC.RIP - Качай редкие курсы!.url", "Прочти перед изучением!.docx"]:
-                    os.remove(new_file_path)
+
         for dir_name in dirs:
             if substring in dir_name:
                 new_dir_name = dir_name.replace(substring, '')
                 old_dir_path = os.path.join(root, dir_name)
                 new_dir_path = os.path.join(root, new_dir_name)
                 os.rename(old_dir_path, new_dir_path)
-                remove_substring_from_filename(new_dir_path, substring)  # Recursively process subdirectories
 
 
-# Specify the folder path and the substring to remove
+def delete_files_by_name(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            if '.url' in file_name or 'прочти перед изучением!' in file_name.lower():
+                file_path = os.path.join(root, file_name)
+                os.remove(file_path)
+
+
+# Specify the folder path and substring to be removed
 folder_path = os.getcwd()
-substring = "[SW.BAND]"
+substring = '[SW.BAND]'
 
-# Remove substring from file names and directory names recursively
+# Remove the substring from filenames
 remove_substring_from_filename(folder_path, substring)
 
-print('\nDone!')
+
+# Delete the specified files
+delete_files_by_name(folder_path)
