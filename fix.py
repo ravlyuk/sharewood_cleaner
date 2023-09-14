ce240d13c16409a4dd62bc4a1ad888dc1bde761d
Yevhenii Ravliuk <41680988+ravlyuk@users.noreply.github.com>
2023-09-14 10:04:02 +0300

    Update fix.py

--- fix.py
+++ fix.py
@@ -1,46 +1,37 @@
 """
-
 RUN FROM COURSE FOLDER:
-
 python3 fix.py
-
 """
 
 import os
 
 
-def remove_substring_from_filename(folder_path, substring):
-    for root, dirs, files in os.walk(folder_path):
+def rename_folders_and_files_recursively(path: str, substr: str) -> None:
+    for root, dirs, files in os.walk(path, topdown=False):
         for file_name in files:
-            if substring in file_name:
-                new_file_name = file_name.replace(substring, '')
-                old_file_path = os.path.join(root, file_name)
-                new_file_path = os.path.join(root, new_file_name)
-                os.rename(old_file_path, new_file_path)
+            file_path = os.path.join(root, file_name)
+            new_file_name = file_name.replace(substr, "")
+            new_file_path = os.path.join(root, new_file_name)
+            os.rename(file_path, new_file_path)
 
-        for dir_name in dirs:
-            if substring in dir_name:
-                new_dir_name = dir_name.replace(substring, '')
-                old_dir_path = os.path.join(root, dir_name)
-                new_dir_path = os.path.join(root, new_dir_name)
-                os.rename(old_dir_path, new_dir_path)
+        for folder_name in dirs:
+            folder_path = os.path.join(root, folder_name)
+            new_folder_name = folder_name.replace(substr, "")
+            new_folder_path = os.path.join(root, new_folder_name)
+            os.rename(folder_path, new_folder_path)
 
 
-def delete_files_by_name(folder_path):
-    for root, dirs, files in os.walk(folder_path):
+def delete_files_by_name(path: str) -> None:
+    for root, dirs, files in os.walk(path):
         for file_name in files:
             if '.url' in file_name or 'прочти перед изучением!' in file_name.lower():
-                file_path = os.path.join(root, file_name)
-                os.remove(file_path)
+                os.remove(os.path.join(root, file_name))
 
 
-# Specify the folder path and substring to be removed
-folder_path = os.getcwd()
+scan_path = os.getcwd()
 substring = '[SW.BAND]'
 
-# Remove the substring from filenames
-remove_substring_from_filename(folder_path, substring)
+rename_folders_and_files_recursively(scan_path, substring)
+delete_files_by_name(scan_path)
 
-
-# Delete the specified files
-delete_files_by_name(folder_path)
+print('Complete!')
